86a80305114d0510a2d9b678c5d78893e93ed13b
sypianBS <sypianbs@gmail.com>
2022-05-15 11:21:50 +0200

    parse response with an XMLParser

--- ConsumingSoapService/ViewController.swift
+++ ConsumingSoapService/ViewController.swift
@@ -23,6 +23,11 @@ class ViewController: UIViewController {
     
     let soapServiceUrl = "http://localhost:8080/ws"
     
+    var xmlDict = [String: Any]()
+    var xmlDictArr = [[String: Any]]()
+    var currentElement = ""
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         consumeSoap()
@@ -37,15 +42,62 @@ class ViewController: UIViewController {
         let session = URLSession.shared
         let task = session.dataTask(with: urlRequest, completionHandler: {data, response, error -> Void in
             let strData = String(data: data!, encoding: .utf8)
-            print(String(describing: (strData)))
+            
+            let xmlParser = XMLParser(data: data!)
+            xmlParser.delegate = self
+            xmlParser.parse()
             
             if error != nil {
                 print(String(describing: (error)))
             }
         })
         task.resume()
-        
     }
 
+}
+
+//parsing idea based on https://dev.to/midhetfatema94/xml-parsing-in-swift-4l8h
+extension ViewController: XMLParserDelegate {
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == "student" {
+               xmlDict = [:]
+           } else {
+               currentElement = elementName
+           }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if !string.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            if xmlDict[currentElement] == nil {
+                xmlDict.updateValue(string, forKey: currentElement)
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "student" {
+            xmlDictArr.append(xmlDict)
+        }
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        let student = Student(details: xmlDict)
+        print(student.id)
+        print(student.name)
+    }
+}
+
+struct Student {
+    let id: Int
+    let name: String
+    
+    init(details: [String: Any]) {
+        if let stringId = details["id"] as? String, let intId = Int(stringId) {
+            id = intId
+        } else {
+            id = -1
+        }
+        name = details["name"] as? String ?? ""
+    }
 }
 
